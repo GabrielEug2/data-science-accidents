c075520556090db0afc4e60258f7c896ede18de3
Renan de Souza Antunes <renandesouzaantunes@gmail.com>
2019-03-30 21:09:04 -0300

    Add alguns graficos

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -5,7 +5,7 @@
 
 
 import pandas as pd
-import seaborn as sb
+import seaborn as sns
 
 
 # In[2]:
@@ -30,10 +30,10 @@ grouped_by_accident = df.groupby('id')
 
 # Se o acidente é o mesmo, essas informações vão ser iguais pra todas as linhas daquele acidente
 accident_data = grouped_by_accident[
-    'data_inversa', 'uf', 'br', 'km', 'municipio', 'causa_acidente',
+    'data_inversa', 'dia_semana', 'horario', 'uf', 'br', 'km', 'municipio', 'causa_acidente',
     'tipo_acidente', 'classificacao_acidente', 'fase_dia', 'sentido_via',
-    'condicao_metereologica', 'tipo_pista', 'tracado_via', 'uso_solo',
-    'latitude', 'longitude', 'regional', 'delegacia', 'uop'
+    'condicao_metereologica', 'tipo_pista', 'tracado_via', 'uso_solo', 'tipo_veiculo',
+    'ano_fabricacao_veiculo', 'latitude', 'longitude', 'regional', 'delegacia', 'uop'
 ].first()
 
 # Calcula a quantidade de feridos, mortos, etc no acidente
@@ -42,7 +42,7 @@ accident_data['n_feridos_leves'] = grouped_by_accident['feridos_leves'].sum()
 accident_data['n_feridos_graves'] = grouped_by_accident['feridos_graves'].sum()
 accident_data['n_mortos'] = grouped_by_accident['mortos'].sum()
 
-accident_data['n_envolvidos'] = accident_data['n_ilesos'] + accident_data['n_feridos_leves'] +                                 accident_data['n_feridos_graves'] + accident_data['n_mortos']
+accident_data['n_envolvidos'] = accident_data['n_ilesos'] + accident_data['n_feridos_leves'] + accident_data['n_feridos_graves'] + accident_data['n_mortos']
 
 accident_data.head()
 
@@ -55,13 +55,58 @@ accident_data.describe()
 
 # In[6]:
 
+# sns.pairplot(accident_data)
 
-# Algumas coisas para plotar
+# In[7]:
 
 # Número de acidentes por dia da semana
+pd.value_counts(accident_data['dia_semana']).plot('bar')
+
+# In[8]:
+
 # Número de acidentes por tipo de acidente
+pd.value_counts(accident_data['tipo_acidente']).plot('bar')
+
+# In[9]:
+
 # Número de acidentes por fase do dia
+pd.value_counts(accident_data['fase_dia']).plot('bar')
+
+# In[10]:
+
 # Número de acidentes por causa
+pd.value_counts(accident_data['causa_acidente']).plot('bar')
 
-sb.pairplot(accident_data)
+# In[11]:
 
+# Série temporal
+sns.lineplot(hue="event", data=accident_data.groupby('data_inversa').size())
+
+# In[12]:
+
+# Acidentes por estado
+pd.value_counts(accident_data['uf']).plot('bar')
+
+# In[13]:
+
+# Acidentes por BR
+pd.value_counts(accident_data['br']).plot('bar')
+
+# In[13]:
+
+# Acidentes por clima
+pd.value_counts(accident_data['condicao_metereologica']).plot('bar')
+
+# In[14]:
+
+# Acidentes por tipo de veiculo
+pd.value_counts(accident_data['tipo_veiculo']).plot('bar')
+
+# In[15]:
+# mortos por ano do veículo (carros mais novos são mais seguros, será?)
+
+# absoluto
+sns.lineplot(hue="event", data=accident_data.groupby('ano_fabricacao_veiculo').agg('sum')['n_mortos'])
+
+# média
+sns.lineplot(hue="event", data=accident_data.groupby('ano_fabricacao_veiculo').agg('mean')['n_mortos'])
