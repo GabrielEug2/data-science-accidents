3f0ad11664cba47c6e2800f6f497aeb13f1893f4
GabrielEug2 <gabriel.eug2@gmail.com>
2019-04-01 21:35:53 -0300

    Add scatterplot latitude x longitude e outro relacionando tipo de pista com tipo de acidente

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -1,34 +1,37 @@
 
 # coding: utf-8
 
-# # Relatório - Análise Exploratória
-# 
-# Gabriel Eugenio Brito, Renan de Souza Antunes, Aurélio Vinicius Cabral Funes
-
 # ## Descrição das fontes de dados
 # 
-# Os dados utilizados são provenientes da Polícia Rodoviária Federal, e estão disponíveis em https://www.prf.gov.br/portal/dados-abertos/acidentes. Eles consistem em uma série de arquivos csv com informações referentes a acidentes ocorridos do ano de 2007 em diante. Optamos por usar apenas os dados de 2018, mas se for  necessário podemos utilizar dados de outros anos para fazer um comparativo.
+# Os dados utilizados são provenientes da Polícia Rodoviária Federal, e estão  disponíveis
+# em https://www.prf.gov.br/portal/dados-abertos/acidentes. Eles consistem em uma série de
+# arquivos csv com informações referentes a acidentes ocorridos do ano de 2007 em diante.
+#
+# Optamos por usar apenas os dados de 2018, mas se for necessário podemos utilizar dados
+# de outros anos para fazer um comparativo.
 
 # In[1]:
-
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
 # In[2]:
-
 
 df = pd.read_csv('acidentes2018.csv', sep=';', encoding='latin-1')
 
-
-# A princípio pensamos que cada linha do csv tivesse informações relativas a um acidente, mas após olhar com mais cuidado percebemos que existem várias linhas com o mesmo id. Também temos colunas como "idade", "sexo" e "estado_fisico", que não fazem sentido quando estamos falando de um acidente.
+# A princípio pensamos que cada linha do csv tivesse informações relativas a um
+# acidente, mas após olhar com mais cuidado percebemos que existem várias linhas
+# com o mesmo id. Também temos colunas como "idade", "sexo" e "estado_fisico",
+# que não fazem sentido quando estamos falando de um acidente.
 # 
-# Acontece que cada linha representa __uma pessoa envolvida__ em um acidente. Então se um acidente envolveu, digamos, 5 pessoas, haverão 5 linhas referentes a este acidente no csv. Podemos ver que há uma certa redundância nos dados, pois algumas das informações como o horário e o local do acidente são as mesmas para todos os envolvidos.
+# Acontece que cada linha representa __uma pessoa envolvida__ em um acidente.
+# Então se um acidente envolveu, digamos, 5 pessoas, haverão 5 linhas referentes
+# a este acidente no csv. Podemos ver que há uma certa redundância nos dados,
+# pois algumas das informações como o horário e o local do acidente são as
+# mesmas para todos os envolvidos.
 
 # In[3]:
-
 
 # Raw data
 pd.set_option('display.max_columns', None) # None = mostra todas
@@ -36,20 +39,21 @@ pd.set_option('display.max_columns', None) # None = mostra todas
 print(df.columns)
 df.head()
 
-
 # As features incluem, entre outras informações:
 # 
-# * __Causa do acidente__: como por exemplo "Defeito Mecânico no Veículo" e "Falta de Atenção à Condução".
-# * __Tipo do acidente__: como "Capotamento" ou "Colisão traseira".
-# * __Local onde aconteceu__: município, BR, km e até mesmo a latitude e longitude de onde aconteceu.
+# * __Causa do acidente__: "Defeito Mecânico no Veículo", "Falta de Atenção à Condução", etc
+# * __Tipo do acidente__: "Capotamento", "Colisão traseira", etc
+# * __Local onde aconteceu__: município, BR e até mesmo a latitude e longitude de onde aconteceu.
 # * __Condição meteorológica__: como estava o clima no momento do acidente.
-# * __Estado físico dos envolvidos__: classificados em "ilesos", "feridos leves", "feridos graves" e "mortos".
+# * __Estado físico dos envolvidos__: "ilesos", "feridos leves", "feridos graves" ou "mortos".
 # * __Tipo do veículo__: automóvel, motocicleta, etc
 # 
-# Optamos por descartar os dados específicos dos envolvidos, e manter apenas as informações relativas ao acidente e aos veículos.
+# Optamos por descartar os dados específicos dos envolvidos, e manter apenas as
+# informações relativas ao acidente e aos veículos.
 
 # In[4]:
 
+df.dropna(inplace=True)
 
 # Agrupa os dados por acidente
 grouped_by_accident = df.groupby('id')
@@ -59,7 +63,7 @@ accident_data = grouped_by_accident[
     'data_inversa', 'dia_semana', 'horario', 'uf', 'br', 'km', 'municipio',
     'causa_acidente', 'tipo_acidente', 'classificacao_acidente', 'fase_dia',
     'sentido_via', 'condicao_metereologica', 'tipo_pista', 'tracado_via',
-    'uso_solo', 'latitude', 'longitude', 'regional', 'delegacia', 'uop'
+    'uso_solo', 'latitude', 'longitude', 'regional'
 ].first()
 
 # Calcula a quantidade de feridos, mortos, etc em cada acidente
@@ -71,9 +75,7 @@ accident_data['n_envolvidos'] = accident_data['n_ilesos'] + accident_data['n_fer
 
 accident_data.head()
 
-
 # In[5]:
-
 
 # Agrupa os dados por veículo
 grouped_by_vehicle = df.groupby('id_veiculo')
@@ -91,70 +93,48 @@ vehicle_data['n_envolvidos'] = vehicle_data['n_ilesos'] + vehicle_data['n_ferido
 
 vehicle_data.head()
 
-
-# ## Análises inciais de distribuição e/ou correlação
-# 
-# A partir destes dados podemos construir diversos gráficos que mostram informações como a principal causa, o tipo mais comum, o estado e a BR com maior número de acidentes.
-
 # In[6]:
-
 
 # Algumas estatísticas
 accident_data.describe()
 
-
 # In[7]:
-
 
 # Número de acidentes por tipo
 plt.figure(figsize=(8,4))
 sns.countplot(y="tipo_acidente", data=accident_data, order=accident_data['tipo_acidente'].value_counts(ascending=True).index)
 
-
 # In[8]:
-
 
 # Número de acidentes por causa
 plt.figure(figsize=(8,6))
 sns.countplot(y="causa_acidente", data=accident_data, order=accident_data['causa_acidente'].value_counts(ascending=True).index)
 
-
 # In[9]:
-
 
 # Número de acidentes por estado
 plt.figure(figsize=(8,8))
 sns.countplot(y="uf", data=accident_data, order=accident_data['uf'].value_counts(ascending=True).index)
 
-
 # In[10]:
-
 
 # Número de acidentes por BR
 plt.figure(figsize=(8,25))
 sns.countplot(y="br", data=accident_data, order=accident_data['br'].value_counts(ascending=True).index)
 
-
-# Também podemos ver a distribuição dos acidentes no decorrer do tempo, o que mostra os períodos com maior número de acidentes.
-
 # In[11]:
-
 
 # Número de acidentes por dia da semana
 plt.figure(figsize=(8,4))
 sns.countplot(x="dia_semana", data=accident_data)
 
-
 # In[12]:
-
 
 # Número de acidentes por fase do dia
 plt.figure(figsize=(8,4))
 sns.countplot(x="fase_dia", data=accident_data, order=['Amanhecer', 'Pleno dia', 'Anoitecer', 'Plena Noite'])
 
-
 # In[13]:
-
 
 # Número de acidentes por hora
 tmp = pd.DataFrame()
@@ -163,9 +143,7 @@ tmp['hora'] = pd.to_datetime(accident_data['horario']).dt.hour
 plt.figure(figsize=(8, 4))
 sns.countplot(x='hora', data=tmp)
 
-
 # In[14]:
-
 
 # Número de acidentes no decorrer do ano (série temporal)
 
@@ -176,33 +154,37 @@ accident_data.sort_values(by='data_inversa')
 plt.figure(figsize=(12, 4))
 sns.lineplot(data=accident_data.groupby('data_inversa').size())
 
-
-# A seguir podemos ver que, contrariando as expectativas, a maior parte dos acidentes não ocorre quando está chovendo, mas sim quando o clima é de céu claro.
-
 # In[15]:
-
 
 # Número de acidentes por clima
 plt.figure(figsize=(12,6))
 sns.countplot(x="condicao_metereologica", data=accident_data, order=accident_data['condicao_metereologica'].value_counts(ascending=True).index)
 
-
 # Não é nenhuma surpresa que automóveis estão envolvidos na maior parte dos acidentes.
 
 # In[16]:
-
 
 # Acidentes por tipo de veiculo
 plt.figure(figsize=(12,8))
 sns.countplot(y="tipo_veiculo", data=vehicle_data, order=vehicle_data['tipo_veiculo'].value_counts().index)
 
-
 # Uma questão que seria interessante explorar é se os carros mais novos são mais seguros, mas não temos dados suficientes para responder isso.
 
 # In[17]:
 
-
 # Número de mortos x ilesos x feridos graves x feridos leves por ano do veículo
 plt.figure(figsize=(12,8))
 sns.lineplot(data=vehicle_data.groupby('ano_fabricacao_veiculo')['n_ilesos', 'n_feridos_leves', 'n_feridos_graves', 'n_mortos'].agg('sum'))
+
+# In[18]:
+
+# Latitude e longitude
+plt.figure(figsize=(8,8))
+sns.scatterplot(x="longitude", y="latitude", data=accident_data)
+
+# In[19]
+
+# Número de acidentes por tipo, para cada tipo de pista
+plt.figure(figsize=(8,12))
+sns.countplot(y='tipo_acidente', hue='tipo_pista', data=accident_data, order=accident_data['tipo_acidente'].value_counts(ascending=True).index)
 
