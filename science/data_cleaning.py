c4c8717f7c8cd7d5ef343b756a4a578567adc716
Renan de Souza Antunes <renandesouzaantunes@gmail.com>
2019-04-15 00:35:43 -0300

    Concatenação e limpeza dos dados de 2017 e 2018 (agrupados por ocorrência)

---
+++ science/data_cleaning.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+
+# data_2017 = pd.read_csv("datatran2017.csv", sep=";", encoding="latin-1")
+# data_2018 = pd.read_csv("datatran2018.csv", sep=";", encoding="latin-1")
+
+# full_data = pd.concat([data_2017, data_2018])
+# full_data.to_csv("full.csv", index=False)
+
+# final = pd.read_csv("final.csv")
+# print(final.describe())
+# print(final.head())
+
+# print("\nRAW DATA DESCRIPTION:\n")
+# print(full_data.describe())
+# print(full_data.head())
+
+def clean_data(df):
+    # remove colunas que não serão utilizadas
+    df.drop(columns=["regional"], inplace=True)
+    df.drop(columns=["delegacia"], inplace=True)
+    df.drop(columns=["uop"], inplace=True)
+
+    # troca células vazias por NaN
+    df.replace("", np.nan, inplace=True)
+
+    # remove qualquer linha com NaN
+    df.dropna(inplace=True)
+
+    # cria coluna timestamp com data + hora
+    df["timestamp"] = pd.to_datetime(df["data_inversa"] + " " + df["horario"])
+    df.drop(columns=["data_inversa"], inplace=True)
+    df.drop(columns=["horario"], inplace=True)
+
+    # reordena as colunas, colocando timestamp na segunda posição
+    cols = df.columns.tolist()
+    cols.insert(1, cols[-1])
+    cols = cols[:-1]
+    df = df[cols]
+
+    # ajusta os tipos
+    df["id"] = df["id"].astype(int)
+    df["br"] = df["br"].astype(int)
+    df["km"].replace(",", ".", regex=True, inplace=True)
+    df["km"] = df["km"].astype(float)
+
+    # print("\nCLEANED DATA:\n")
+    # print(df.describe())
+    # print(df.head())
+
+    df.to_csv("final.csv", index=False)
+
+
+# clean_data(full_data)
